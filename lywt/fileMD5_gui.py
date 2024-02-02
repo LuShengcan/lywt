e0ace0565a94c6188e0c3fc0664cd1b4ce99d4a8
Lu Shengcan <lu.shengcan@zte.com.cn>
2024-02-02 17:20:10 +0800

    11

--- lywt/fileMD5_gui.py
+++ lywt/fileMD5_gui.py
@@ -5,30 +5,32 @@ import hashlib
 import tkinter as tk
 import windnd
 
+class App(tk.Tk):
+    def __init__(self) -> None:
+        super().__init__()
+        self.make_window()
 
-def drag_files(files):
-    for file in files:
-        file = file.decode('gbk')
-        text_1.insert(tk.END, file+'\n')
-        with open(file, 'rb') as fr:
-            text_2.insert(tk.END, hashlib.md5(fr.read()).hexdigest()+'\n')
+    def make_window(self) -> None:
+        self.geometry("900x600")
+        self.title('文件MD5获取')
+        label1 = tk.Label(self, text='请拖拽文件')
+        text_1 = tk.Text(self, width=50, height=20, font=("黑体", 10, ""))
+        text_2 = tk.Text(self, width=35, height=10, font=("黑体", 18, ""))
+
+        label1.grid(row=0, column=0)
+        text_1.grid(row=0, column=1)
+        text_2.grid(row=0, column=2)
+
+        windnd.hook_dropfiles(self, func=self.drag_files())
+
+    def drag_files(self, files, entry: tk.Entry) -> None:
+        for file in files:
+            file = file.decode('gbk')
+            entry.insert(tk.END, file+'\n')
+            with open(file, 'rb') as fr:
+                entry.insert(tk.END, hashlib.md5(fr.read()).hexdigest()+'\n')
 
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    root.geometry("900x300")
-
-    # 组件
-    label1 = tk.Label(root, text='请拖拽文件')
-    text_1 = tk.Text(root, width=50, height=20, font=("黑体", 10, ""))
-    text_2 = tk.Text(root, width=35, height=10, font=("黑体", 18, ""))
-
-    # 布局
-    label1.place(x=0, y=10)
-    text_1.place(x=100, y=0)
-    text_2.place(x=460, y=0)
-
-    windnd.hook_dropfiles(root, func=drag_files)
-    print('hello world')
-
-    root.mainloop()
+    app = App()
+    app.mainloop()
