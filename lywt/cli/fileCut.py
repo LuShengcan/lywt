ca971525103c740488ae8e56c9596cd46567e27d
Lu Shengcan <lu.shengcan@zte.com.cn>
2024-03-01 15:15:21 +0800

    完善文件分割

--- lywt/cli/fileCut.py
+++ lywt/cli/fileCut.py
@@ -1,31 +1,58 @@
+from typing import IO
+import os
+
 """
-    这是一个文件处理的包，目前有的功能:
-        splitBigFile: 将大文件按每个文件 linesPerFile 行分割成若干个小文件
+这是一个文件处理的包，目前有的功能:
+    splitByLines: 将大文件按每个文件 linesPerFile 行分割成若干个小文件
+
+    
 """
 
 
-def splitBigFile(src: str, dest: str, linesPerFile=100000, encoding: str = 'utf-8'):
-    suffix = src.split('.')[-1]
+def splitByLines(src: str, dst: str, linesPerFile=100000, encoding='utf-8'):
+    '''
+    scr: 源文件
+    dst: 存储目录
+    linesPerfile: 每个小文件的行数
+    encoding: 编码
+    '''
+
+    # 确定要解析的文件
+    if not os.path.exists(src):
+        print(f"path '{src}' not exists!")
+        return
+
+    # 确定存储目录
+    if os.path.exists(dst):
+        if not os.path.isdir(dst):
+            print(f'dst:{dst} is not a dst!')
+            return
+    else:
+        os.mkdir(dst)
+
+    # 开始处理
+    numOfLine = 0   # 总行数
+    numOfFile = 0   # 生成的文件个数
+    fw: IO = None
     with open(src, 'r', encoding=encoding) as fr:
-        lineNum = 0
-        fileNum = 0
-        lines = []
-        while 1:
-            if lineNum < linesPerFile:
-                line = fr.readline()
-                if not line:
-                    if lineNum == 0:
-                        return
-                    else:
-                        with open(dest + '/split_file_' + str(fileNum) + '.' + suffix, 'w', encoding=encoding) as fw:
-                            fw.writelines(lines)
-                        return
-                else:
-                    lines.append(line)
-                    lineNum += 1
+        for line in fr:
+            if numOfLine % linesPerFile == 0:
+                if fw:
+                    fw.close()
+                fileName = os.path.basename(src)
+                prefix = os.path.splitext(fileName)[0]
+                suffix = os.path.splitext(fileName)[1]
+                filePath = os.path.join(dst, f'{prefix}_{numOfFile}{suffix}')
+                fw = open(filePath, 'w', encoding=encoding)
+                numOfFile += 1
+
+                fw.write(line)
             else:
-                with open(dest + '/split_file_' + str(fileNum) + '.' + suffix, 'w', encoding=encoding) as fw:
-                    fw.writelines(lines)
-                lineNum = 0
-                fileNum += 1
-                lines = []
+                fw.write(line)
+            numOfLine += 1
+        if fw:
+            fw.close()
+    print(f"总行数{numOfLine}，每个文件{linesPerFile}行，总共生成了{numOfFile}个文件")
+
+def splitBySize(src: str, dst:str, sizePerFile=1):
+    pass
