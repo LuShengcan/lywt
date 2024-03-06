4909d6e8889d56c0f3328153f67bf89837935ffa
Lu Shengcan <lu.shengcan@zte.com.cn>
2024-03-06 12:05:25 +0800

    update

--- lywt/cli/textProcess.py
+++ lywt/cli/textProcess.py
@@ -1,9 +1,10 @@
-# 16进制按字节转换为字符
-def HexByte2Char(txt: str):
+
+
+def Hex2Char(txt: str):
     chars = [chr(int(txt[i:i + 2], 16)) for i in range(0, len(txt), 2)]
     return ''.join(chars)
 
 
 if __name__ == '__main__':
-    txt = '73'
-    print(HexByte2Char(txt))
+    txt = '4b0b'
+    print(Hex2Char(txt))
