b645e44adecc1e21438b7da8225df1317bc9af58
Lu Shengcan <lu.shengcan@zte.com.cn>
2024-09-14 08:46:11 +0800

    修改build脚本

--- build.py
+++ build.py
@@ -1,27 +1,28 @@
 import subprocess
 import os
 import re
+from datetime import datetime
 
 
-def get_small_version():
+def get_small_version(update):
     max_small_version = -1
 
     for file in os.listdir('dist'):
-        match = re.match('.*?_v(\\d+)\\.(\\d+)', file)
+        match = re.match('.*?v(\\d+)\\.(\\d+).*?', file)
         if match:
             max_small_version = max(max_small_version, int(match.group(2)))
-
-    return max_small_version + 1
+    if update:
+        return max_small_version + 1
+    else:
+        return max_small_version
 
 
 if __name__ == '__main__':
     APP_NAME = 'lywt'
     BIG_VERSION = '1'
-    SMALL_VERSION = str(get_small_version())
-    VERSION = APP_NAME + BIG_VERSION + '.' + SMALL_VERSION
+    SMALL_VERSION = str(get_small_version(update=0))
+    BUILD_TIME = datetime.now().strftime(r'%Y%m%d%H%M')
+    VERSION = APP_NAME + '_v' + BIG_VERSION + '.' + SMALL_VERSION + '.' + BUILD_TIME
 
-    # 设置 PyInstaller 命令
     pyinstaller_command = f'pyinstaller -n {VERSION} --noconsole main.py'
-
-    # 执行 PyInstaller 命令
     subprocess.run(pyinstaller_command, shell=True)
