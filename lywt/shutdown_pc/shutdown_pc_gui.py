9b09ed8aa6587185214edb609ea3252d8bad5b77
Lu Shengcan <lu.shengcan@zte.com.cn>
2024-11-05 17:17:30 +0800

    更新shutdown pc

--- lywt/shutdown_pc/shutdown_pc_gui.py
+++ lywt/shutdown_pc/shutdown_pc_gui.py
@@ -3,23 +3,16 @@ from tkinter import ttk
 from tkinter import StringVar
 from .shutdown_pc_cli import shut_down_at, shutdown_after
 
+
 class ShutdownPC(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
         self.creat_window()
         self.creat_vars()
         self.create_widgets()
-        self.grid_widgets()
 
     def creat_window(self):
         self.title('定时关机')
-        self.window_width = 400
-        self.window_height = 200
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
-        x = (screen_width - self.window_width) // 2
-        y = (screen_height - self.window_height) // 2
-        self.geometry(f"{self.window_width}x{self.window_height}+{x}+{y}")
 
     def creat_vars(self):
         self.hour = StringVar(value='00')
@@ -27,22 +20,19 @@ class ShutdownPC(tk.Toplevel):
         self.radio_var = tk.IntVar(value=1)
 
     def create_widgets(self):
-        self.combobox_hour = ttk.Combobox(self, values=[f"{i:02d}" for i in range(24)], textvariable=self.hour)
-        self.combobox_minute = ttk.Combobox(self, values=[f"{i:02d}" for i in range(60)], textvariable=self.minute)
+        self.f0 = ttk.Frame(self)
+        self.f0.pack(side=tk.TOP, padx=2, pady=2)
 
-        self.radio_1 = ttk.Radiobutton(self, text="指定关机时间", variable=self.radio_var, value=1)
-        self.radio_2 = ttk.Radiobutton(self, text="多长时间后关机", variable=self.radio_var, value=2)
+        ttk.Label(self.f0, text="时间/时长选择:").grid(row=0, column=0, padx=2, pady=2)
+        ttk.Combobox(self.f0, values=[f"{i:02d}" for i in range(24)], textvariable=self.hour).grid(row=0, column=1, padx=2, pady=2)
+        ttk.Combobox(self.f0, values=[f"{i:02d}" for i in range(60)], textvariable=self.minute).grid(row=0, column=2, padx=2, pady=2)
 
-        self.button_shutdown = ttk.Button(self, text='创建计划', command=self.make)
+        ttk.Label(self.f0, text="关机类型:").grid(row=1, column=0, padx=2, pady=2)
+        ttk.Radiobutton(self.f0, text="指定关机时间", variable=self.radio_var, value=1).grid(row=1, column=1, padx=2, pady=2)
+        ttk.Radiobutton(self.f0, text="多长时间后关机", variable=self.radio_var, value=2).grid(row=1, column=2, padx=2, pady=2)
 
-    def grid_widgets(self):
-        self.combobox_hour.grid(row=0, column=0)
-        self.combobox_minute.grid(row=0, column=1)
-
-        self.radio_1.grid(row=1, column=0)
-        self.radio_2.grid(row=1, column=1)
-
-        self.button_shutdown.grid(row=2, column=0)
+        ttk.Button(self.f0, text='创建计划', command=self.make).grid(row=2, column=0, padx=2, pady=2)
+        ttk.Button(self.f0, text='取消计划', command=self.cancel).grid(row=2, column=1, padx=2, pady=2)
 
     def make(self):
         if self.radio_var.get() == 1:
@@ -51,3 +41,6 @@ class ShutdownPC(tk.Toplevel):
             shutdown_after(int(self.hour.get()), int(self.minute.get()))
         else:
             pass
+
+    def cancel(self):
+        pass
